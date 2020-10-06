49b2de796ecc0f7017e19e0f2456080f2245b25a
DarioQuintanilla2109 <dquin106@fiu.edu>
2020-10-06 22:19:26 +0000

    added countdown to user input feature

--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,32 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let userInput = prompt("Enter a number");
+
+var counter = userInput;
+var newCounter;
+let stopLoop = true;
+
+function increaseCounter() {
+	counter--;
+	newCounter = "000000" + counter;
+	newCounter = newCounter.slice(-6);
+
+	ReactDOM.render(
+		<Home
+			number1={newCounter.charAt(0)}
+			number2={newCounter.charAt(1)}
+			number3={newCounter.charAt(2)}
+			number4={newCounter.charAt(3)}
+			number5={newCounter.charAt(4)}
+			number6={newCounter.charAt(5)}
+		/>,
+		document.querySelector("#app")
+	);
+	if (newCounter === "000000") {
+		clearInterval(flag);
+		console.log(flag);
+	}
+}
+
+let flag = setInterval(increaseCounter, 1000);
